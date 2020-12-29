4591787405d295230631be8b72fda97de63c91aa
shinora008 <k.shinozaki8@gmail.com>
2020-12-29 16:14:05 +0900

    Add bootstrap

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby '2.6.6'
 gem 'rails', '~> 6.0.3'
 gem 'slim'
 gem 'html2slim'
-
+gem 'bootstrap-sass'
 # Use sqlite3 as the database for Active Record
 
 # Use Puma as the app server
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'なか食レポ'
+    if page_title.blank?
+      else
+        "#{page_title} - #{bese_title}"
+      end
+    end
 end

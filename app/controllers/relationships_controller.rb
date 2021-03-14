fc3d486b89c2e90e97ca1fe8aa619337fe7cb6d5
shinora008 <k.shinozaki8@gmail.com>
2021-03-15 05:14:00 +0900

    fix_heroku

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,20 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
-  def create
-    @user = User.find(params[:favorites_id])
-    current_user.favorite(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-  end
+    def create
+      @user = User.find(params[:favorites_id])
+      current_user.favorite(@user)
+      respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+    end
 
-  def destroy
-    @user = User.find(params[:id]).favorite
-    current_user.unfavorite(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+    def destroy
+      @user = User.find(params[:id]).favorite
+      current_user.unfavorite(@user)
+      respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
+    end
   end
-end
